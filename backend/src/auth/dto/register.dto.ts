9ce5d11e6b71d1432fb65c013845c4ba214bde4c
Filip Jovanovic <filipjovanovic107@gmail.com>
2026-06-17 13:04:14 +0200

    dodat registerDto

---
+++ backend/src/auth/dto/register.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  username: string;
+
+  @IsString()
+  city?: string;
+
+  @IsString()
+  type?: string; // 'gamer', 'sports', 'both'
+}
