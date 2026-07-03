d70a0f1f81c51968889e292cd9428afd3c93014d
Filip Jovanovic <filipjovanovic107@gmail.com>
2026-07-03 13:25:06 +0200

    dodat usersController

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Put, Delete, Param, Body, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getMe(@Request() req: { user: { userId: number } }) {
+    return this.usersService.findOne(req.user.userId);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put(':id')
+  update(@Param('id') id: string, @Body() data: Partial<User>) {
+    return this.usersService.update(+id, data);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get(':id/friends')
+  getFriends(@Param('id') id: string) {
+    return this.usersService.getFriends(+id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put(':id/friends/:friendId')
+  addFriend(@Param('id') id: string, @Param('friendId') friendId: string) {
+    return this.usersService.addFriend(+id, +friendId);
+  }
+}
