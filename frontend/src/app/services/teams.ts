f0cd5d825c435bca70a6ca53eb36ac7cb82bd11e
Filip Jovanovic <filipjovanovic107@gmail.com>
2026-07-07 01:35:45 +0200

    dodati teamsService i usersService

--- frontend/src/app/services/teams.ts
+++ frontend/src/app/services/teams.ts
@@ -1,4 +1,44 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Team } from '../models/team.model';
 
-@Service()
-export class Teams {}
+@Injectable({
+  providedIn: 'root',
+})
+export class TeamsService {
+  private apiUrl = 'http://localhost:3000/teams';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(filters?: { category?: string; city?: string }): Observable<Team[]> {
+    let params = new HttpParams();
+    if (filters?.category) params = params.set('category', filters.category);
+    if (filters?.city) params = params.set('city', filters.city);
+    return this.http.get<Team[]>(this.apiUrl, { params });
+  }
+
+  getOne(id: number): Observable<Team> {
+    return this.http.get<Team>(`${this.apiUrl}/${id}`);
+  }
+
+  create(data: Partial<Team>): Observable<Team> {
+    return this.http.post<Team>(this.apiUrl, data);
+  }
+
+  update(id: number, data: Partial<Team>): Observable<Team> {
+    return this.http.put<Team>(`${this.apiUrl}/${id}`, data);
+  }
+
+  remove(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  joinTeam(teamId: number): Observable<Team> {
+    return this.http.post<Team>(`${this.apiUrl}/${teamId}/members`, {});
+  }
+
+  leaveTeam(teamId: number, userId: number): Observable<Team> {
+    return this.http.delete<Team>(`${this.apiUrl}/${teamId}/members/${userId}`);
+  }
+}
--- frontend/src/app/services/users.ts
+++ frontend/src/app/services/users.ts
@@ -1,4 +1,41 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
 
-@Service()
-export class Users {}
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  private apiUrl = 'http://localhost:3000/users';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
+
+  getMe(): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/me`);
+  }
+
+  getOne(id: number): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/${id}`);
+  }
+
+  update(id: number, data: Partial<User>): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/${id}`, data);
+  }
+
+  remove(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  getFriends(id: number): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/${id}/friends`);
+  }
+
+  addFriend(userId: number, friendId: number): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/${userId}/friends/${friendId}`, {});
+  }
+}
