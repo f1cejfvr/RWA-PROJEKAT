a8e0710eb3b5c687347d514320db9aafc38c64bd
Filip Jovanovic <filipjovanovic107@gmail.com>
2026-07-09 14:57:55 +0200

    dodat NgRx auth store - effects i selectors

---
+++ frontend/src/app/store/auth.effects.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { switchMap, map, catchError, tap } from 'rxjs/operators';
+import { AuthService } from '../services/auth';
+import { login, loginSuccess, loginFailure, register, registerSuccess, registerFailure, logout } from './auth.actions';
+
+@Injectable()
+export class AuthEffects {
+  login$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(login),
+      switchMap(({ email, password }) =>
+        this.authService.login({ email, password }).pipe(
+          map((response) => loginSuccess({ token: response.access_token, user: response.user })),
+          catchError((error) => of(loginFailure({ error: error.message }))),
+        ),
+      ),
+    ),
+  );
+
+  register$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(register),
+      switchMap(({ email, password, username, city, type }) =>
+        this.authService.register({ email, password, username, city, type }).pipe(
+          map((response) => registerSuccess({ token: response.access_token, user: response.user })),
+          catchError((error) => of(registerFailure({ error: error.message }))),
+        ),
+      ),
+    ),
+  );
+
+  loginSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(loginSuccess, registerSuccess),
+        tap(() => this.router.navigate(['/'])),
+      ),
+    { dispatch: false },
+  );
+
+  logout$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(logout),
+        tap(() => {
+          this.authService.logout();
+          this.router.navigate(['/login']);
+        }),
+      ),
+    { dispatch: false },
+  );
+
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+}
