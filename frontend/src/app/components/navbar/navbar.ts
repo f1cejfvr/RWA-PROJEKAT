5e989a48e9f1dcc08b5c4a17caf13d9d5e387f5b
Filip Jovanovic <filipjovanovic107@gmail.com>
2026-07-09 22:54:54 +0200

    dodata navbar komponenta

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { selectIsLoggedIn, selectUser } from '../../store/auth.selectors';
+import { logout } from '../../store/auth.actions';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, AsyncPipe, NgIf],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar implements OnInit {
+  isLoggedIn$: Observable<boolean>;
+  user$: Observable<{ id: number; email: string; username: string } | null>;
+
+  constructor(private store: Store, private router: Router) {
+    this.isLoggedIn$ = this.store.select(selectIsLoggedIn);
+    this.user$ = this.store.select(selectUser);
+  }
+
+  ngOnInit(): void {}
+
+  onLogout(): void {
+    this.store.dispatch(logout());
+  }
+}
