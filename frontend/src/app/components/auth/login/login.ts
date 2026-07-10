876abe39cfc8bfb26de3d7789cd3d3e0fa0bf474
Filip Jovanovic <filipjovanovic107@gmail.com>
2026-07-10 21:47:32 +0200

    dodata login komponenta

--- frontend/src/app/components/auth/login/login.ts
+++ frontend/src/app/components/auth/login/login.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgIf, AsyncPipe } from '@angular/common';
+import { login } from '../../../store/auth.actions';
+import { selectAuthError } from '../../../store/auth.selectors';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterLink, NgIf, AsyncPipe],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  email = '';
+  password = '';
+  error$;
+
+  constructor(private store: Store) {
+    this.error$ = this.store.select(selectAuthError);
+  }
+
+  onSubmit(): void {
+    if (this.email && this.password) {
+      this.store.dispatch(login({ email: this.email, password: this.password }));
+    }
+  }
+}
