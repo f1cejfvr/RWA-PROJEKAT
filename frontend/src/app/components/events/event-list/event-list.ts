551d85f56d64f19f1e1210eb5f711c2c0b887f83
Filip Jovanovic <filipjovanovic107@gmail.com>
2026-07-11 11:04:48 +0200

    dodata eventList komponenta

--- frontend/src/app/components/events/event-list/event-list.ts
+++ frontend/src/app/components/events/event-list/event-list.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { AsyncPipe, NgIf, NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { loadEvents } from '../../../store/events.actions';
+import { selectAllEvents, selectEventsLoading } from '../../../store/events.selectors';
+import { Event } from '../../../models/event.model';
+import { selectIsLoggedIn } from '../../../store/auth.selectors';
 
 @Component({
   selector: 'app-event-list',
-  imports: [],
+  imports: [AsyncPipe, NgIf, NgFor, RouterLink, FormsModule],
   templateUrl: './event-list.html',
   styleUrl: './event-list.css',
 })
-export class EventList {}
+export class EventList implements OnInit, OnDestroy {
+  events$: Observable<Event[]>;
+  loading$: Observable<boolean>;
+  isLoggedIn$: Observable<boolean>;
+  private destroy$ = new Subject<void>();
+
+  categoryFilter = '';
+  cityFilter = '';
+  typeFilter = '';
+
+  constructor(private store: Store) {
+    this.events$ = this.store.select(selectAllEvents);
+    this.loading$ = this.store.select(selectEventsLoading);
+    this.isLoggedIn$ = this.store.select(selectIsLoggedIn);
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(loadEvents({}));
+  }
+
+  applyFilters(): void {
+    this.store.dispatch(loadEvents({
+      filters: {
+        category: this.categoryFilter || undefined,
+        city: this.cityFilter || undefined,
+        type: this.typeFilter || undefined,
+      }
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
