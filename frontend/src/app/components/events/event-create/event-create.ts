6c12716e8bab9e8add03a9e352f0aa9f0a004103
Filip Jovanovic <filipjovanovic107@gmail.com>
2026-07-11 23:52:37 +0200

    dodata eventCreate komponenta

--- frontend/src/app/components/events/event-create/event-create.ts
+++ frontend/src/app/components/events/event-create/event-create.ts
@@ -1,9 +1,46 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { createEvent } from '../../../store/events.actions';
 
 @Component({
   selector: 'app-event-create',
-  imports: [],
+  imports: [FormsModule, NgIf, RouterLink],
   templateUrl: './event-create.html',
   styleUrl: './event-create.css',
 })
-export class EventCreate {}
+export class EventCreate {
+  title = '';
+  description = '';
+  type = 'casual';
+  category = 'gaming';
+  game = '';
+  sport = '';
+  city = '';
+  location = '';
+  maxPlayers = 2;
+  date = '';
+
+  constructor(private store: Store) {}
+
+  onSubmit(): void {
+    if (this.title && this.category && this.type) {
+      this.store.dispatch(createEvent({
+        data: {
+          title: this.title,
+          description: this.description,
+          type: this.type,
+          category: this.category,
+          game: this.game || undefined,
+          sport: this.sport || undefined,
+          city: this.city || undefined,
+          location: this.location || undefined,
+          maxPlayers: this.maxPlayers,
+          date: this.date ? new Date(this.date) : undefined,
+        }
+      }));
+    }
+  }
+}
