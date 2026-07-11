b848b1a8015d8ae47e2f0d5e0b2386cffb32c80b
Filip Jovanovic <filipjovanovic107@gmail.com>
2026-07-11 10:57:26 +0200

    dodata register komponenta

--- frontend/src/app/components/auth/register/register.ts
+++ frontend/src/app/components/auth/register/register.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgIf, AsyncPipe } from '@angular/common';
+import { register } from '../../../store/auth.actions';
+import { selectAuthError } from '../../../store/auth.selectors';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, RouterLink, NgIf, AsyncPipe],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  email = '';
+  password = '';
+  username = '';
+  city = '';
+  type = 'both';
+  error$;
+
+  constructor(private store: Store) {
+    this.error$ = this.store.select(selectAuthError);
+  }
+
+  onSubmit(): void {
+    if (this.email && this.password && this.username) {
+      this.store.dispatch(register({
+        email: this.email,
+        password: this.password,
+        username: this.username,
+        city: this.city,
+        type: this.type,
+      }));
+    }
+  }
+}
